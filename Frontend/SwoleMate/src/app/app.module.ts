4269d2c96eba228c71349a4b0936cd96e223f3d0
herringtonca <herringtonca@mymail.vcu.edu>
2020-01-29 14:16:45 -0500

    added registration-page component

--- Frontend/SwoleMate/src/app/app.module.ts
+++ Frontend/SwoleMate/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule
