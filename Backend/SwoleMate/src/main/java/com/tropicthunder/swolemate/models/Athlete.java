abe4e0e35c3c371eead272e96bf0ec0eff654740
herringtonca <herringtonca@mymail.vcu.edu>
2020-01-30 15:57:18 -0500

    added coach class in models folder

---
+++ Backend/SwoleMate/src/main/java/com/tropicthunder/swolemate/models/Athlete.java
@@ -0,0 +1,11 @@
+package com.tropicthunder.swolemate.models;
+
+public class Athlete {
+
+  public Athlete() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  
+}
---
+++ Backend/SwoleMate/src/main/java/com/tropicthunder/swolemate/models/Coach.java
@@ -0,0 +1,5 @@
+package com.tropicthunder.swolemate.models;
+
+public class Coach {
+
+}
