faf51f51380e01609fe0138d9d2e6d1761917c1e
unknown <dxrvalley@gmail.com>
2020-01-30 21:45:39 -0500

    dashboard

--- Frontend/SwoleMate/src/app/app.module.ts
+++ Frontend/SwoleMate/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { RegistrationPageComponent } from "./registration-page/registration-page
 import { AppRoutingModule } from "./app-routing.module";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { CoachDashboardComponent } from './coach-dashboard/coach-dashboard.component';
+import { AthleteDashboardComponent } from './athlete-dashboard/athlete-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationPageComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CoachDashboardComponent,
+    AthleteDashboardComponent
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
