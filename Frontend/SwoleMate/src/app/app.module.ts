bbe955a07193101c2ec65f2c398f581e930712dc
unknown <dxrvalley@gmail.com>
2020-01-30 23:08:36 -0500

    Added Messagebox

--- Frontend/SwoleMate/src/app/app.module.ts
+++ Frontend/SwoleMate/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { CoachDashboardComponent } from './coach-dashboard/coach-dashboard.component';
 import { AthleteDashboardComponent } from './athlete-dashboard/athlete-dashboard.component';
+import { MessageBoardComponent } from './message-board/message-board.component';
+import { AthleteMessageboxComponent } from './athlete-messagebox/athlete-messagebox.component';
+import { CoachMessageboxComponent } from './coach-messagebox/coach-messagebox.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { AthleteDashboardComponent } from './athlete-dashboard/athlete-dashboard
     NavbarComponent,
     FooterComponent,
     CoachDashboardComponent,
-    AthleteDashboardComponent
+    AthleteDashboardComponent,
+    MessageBoardComponent,
+    AthleteMessageboxComponent,
+    CoachMessageboxComponent
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
---
+++ Frontend/SwoleMate/src/app/coach-messagebox/coach-messagebox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coach-messagebox',
+  templateUrl: './coach-messagebox.component.html',
+  styleUrls: ['./coach-messagebox.component.css']
+})
+export class CoachMessageboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
