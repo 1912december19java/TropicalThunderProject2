a49a802d243e3dacf7ba0fd70d3c608afe189db2
herringtonca <herringtonca@mymail.vcu.edu>
2020-02-03 11:33:32 -0500

    debugged coach workout page

--- Frontend/SwoleMate/src/app/coach-workout/coach-workout.component.ts
+++ Frontend/SwoleMate/src/app/coach-workout/coach-workout.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Exercise } from "../exercise";
-import { ProgramFormComponent } from '../program-form/program-form.component';
+import { WorkoutService } from '../workout.service';
 
 @Component({
   selector: 'app-coach-workout',
@@ -17,9 +17,8 @@ export class CoachWorkoutComponent implements OnInit {
     frequency: []
   });
   lengthInWeeks: Array<Exercise>[] = [];
-  pfc: ProgramFormComponent = new ProgramFormComponent();
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private workoutService: WorkoutService) { }
   workoutForm = this.fb.group({workout: this.fb.array([])});
   get workout() {
     return this.workoutForm.get('workout') as FormArray;
@@ -31,7 +30,7 @@ export class CoachWorkoutComponent implements OnInit {
     for (let index = 0; index < this.layoutForm.get('lengthInWeeks').value; index++){
         this.lengthInWeeks.push(new Array<Exercise> ())
     }
-    ProgramFormComponent.updateFrequency(this.layoutForm.get('frequency').value);
+    this.workoutService.setFrequency(this.layoutForm.get('frequency').value);
   }
 
   ngOnInit() {
