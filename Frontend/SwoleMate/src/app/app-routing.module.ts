4a5c0a4ed61a895b7f427f5c14a7d41ebe75cdb7
joedonline <contact@joedomingo.online>
2020-02-04 20:59:58 -0500

    Routing config, first layout

--- Frontend/SwoleMate/src/app/app-routing.module.ts
+++ Frontend/SwoleMate/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 import { CoachDashboardComponent } from './coach-dashboard/coach-dashboard.component';
 import { AthleteDashboardComponent } from './athlete-dashboard/athlete-dashboard.component';
 
 const routes: Routes = [
-  { path: 'coach-dashboard', component: CoachDashboardComponent },
-  { path: 'athlete-dashboard', component: AthleteDashboardComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'coach',
+    component: CoachDashboardComponent,
+  },
+  {
+    path: 'athlete',
+    component: AthleteDashboardComponent,
+  },
 ];
 
 @NgModule({
