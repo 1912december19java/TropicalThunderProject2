135f875be4c89634b8e5b739d662fea2b7af968a
unknown <dxrvalley@gmail.com>
2020-02-05 10:44:42 -0500

    save

--- Frontend/SwoleMate/src/app/athlete-dashboard/athlete-dashboard.component.ts
+++ Frontend/SwoleMate/src/app/athlete-dashboard/athlete-dashboard.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AthleteDashboardComponent implements OnInit {
 
+  isCollapsed: Boolean = false;
+  
+
   constructor() { }
 
   ngOnInit() {
