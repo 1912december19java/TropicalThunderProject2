53cf2a9e16189aedcb43db4da92f158af08222ec
unknown <dxrvalley@gmail.com>
2020-02-06 13:29:55 -0500

    k

--- Frontend/SwoleMate/src/app/exercise.ts
+++ Frontend/SwoleMate/src/app/exercise.ts
@@ -1,39 +1,23 @@
 export class Exercise {
-<<<<<<< HEAD
-  ExerciseId?: number;
-=======
-<<<<<<< HEAD
-    programId?:number;
-    sets:number;
-    reps:number;
-    load:string;
-    name:string;
-    day:number;
-    coachNotes:string;
-    athleteNotes?:string;
-    exerciseUrl?:string;
-    isComplete?:boolean;
 
-    constructor(name:string, sets:number, reps:number, load:string, day:number){
-        this.name = name;
-        this.sets = sets;
-        this.reps = reps;
-        this.load = load;
-        this.day = day;
-     }
-=======
->>>>>>> Development
+  ExerciseId?: number;
+
   programId?: number;
-  sets?: number;
-  reps?: number;
-  load?: number;
-  name?: string;
-  day?: number;
-  coachNotes?: string;
+  sets: number;
+  reps: number;
+  load: string;
+  name: string;
+  day: number;
+  coachNotes: string;
   athleteNotes?: string;
   exerciseUrl?: string;
   isComplete?: boolean;
 
-  constructor() {}
->>>>>>> Development
+  constructor(name: string, sets: number, reps: number, load: string, day: number) {
+    this.name = name;
+    this.sets = sets;
+    this.reps = reps;
+    this.load = load;
+    this.day = day;
+  }
 }
