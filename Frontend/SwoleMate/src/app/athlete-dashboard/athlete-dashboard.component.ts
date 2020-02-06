85a2aaf732c567ce4a3bf11a77e3094e0383ea8a
unknown <dxrvalley@gmail.com>
2020-02-05 19:45:38 -0500

    work

--- Frontend/SwoleMate/src/app/athlete-dashboard/athlete-dashboard.component.ts
+++ Frontend/SwoleMate/src/app/athlete-dashboard/athlete-dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { athlete } from "../athlete";
+import { AthleteService } from "../athlete.service"
+import { Observable } from "rxjs";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-athlete-dashboard',
@@ -8,13 +13,32 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 })
 export class AthleteDashboardComponent implements OnInit {
 
+  athlete: Observable<athlete[]>;
+
   isCollapsed: Boolean = false;
   
-  public isCollapsed = false;
+  
 
-  constructor() { }
+  constructor(private athleteService: AthleteService, private router: Router) { }
 
   ngOnInit() {
+    this.reloadData();
   }
+
+  reloadData() {
+    this.athlete = this.athleteService.getAthletesList();
+  }
+
+  denyAthlete(id: number) {
+    this.athleteService.dentAthlete(id).subscribe(
+      data => {
+        console.log(data);
+        this.reloadData();
+      },
+      error => console.log(error));
+    
+  }
+
+  
 
 }
---
+++ Frontend/SwoleMate/src/app/athlete.ts
@@ -0,0 +1,7 @@
+export class athlete {
+    Athlete_id: number;
+    coach: number;
+    Name: string;
+    Email: string;
+    password: string;
+}
