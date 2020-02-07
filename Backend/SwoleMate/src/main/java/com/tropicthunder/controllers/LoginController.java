5f0ef3974bca8dcbc790cae860d80129f3badad1
Joe Domingo <contact@joedomingo.online>
2020-02-07 15:07:26 -0500

    Login authentication logic fixed

--- Backend/SwoleMate/src/main/java/com/tropicthunder/controllers/LoginController.java
+++ Backend/SwoleMate/src/main/java/com/tropicthunder/controllers/LoginController.java
@@ -1,20 +1,14 @@
 package com.tropicthunder.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tropicthunder.models.Athlete;
 import com.tropicthunder.models.Coach;
-import com.tropicthunder.models.Exercise;
 import com.tropicthunder.services.AthleteService;
 import com.tropicthunder.services.CoachService;
 
@@ -24,39 +18,40 @@ import com.tropicthunder.services.CoachService;
 public class LoginController {
 
 	@Autowired
-	private AthleteService aService;
+	private AthleteService athleteService;
 
 	@Autowired
-	private CoachService cService;
+	private CoachService coachService;
 
 	@GetMapping
 	public String getItem(@RequestParam String user, @RequestParam String email, @RequestParam String password) {
 		switch (user) {
 		case "Athlete":
-			return matchPassword(email, password) ? authAthlete(email).getName()
+			System.out.println("[LoginController] : this.matchPassword(email, password) : " + this.matchPassword(email, password));
+			return this.matchPassword(email, password) 
+					? this.getAthlete(email).toString()
 					: "{ \"message\": \"Email or password invalid\" }";
 		case "Coach":
-			return matchPassword(email, password) ? authCoach(email).toString()
+			return this.matchPassword(email, password) 
+					? this.getCoach(email).toString()
 					: "{ \"message\": \"Email or password invalid\" }";
 		default:
-			return "{ \"message\": \"Something wrong while fetching data\" }";
-
+			return "{ \"message\": \"Something went wrong while fetching data\" }";
 		}
 	}
 
-	private Athlete authAthlete(String email) {
-		return aService.getByEmail(email);
+	private Athlete getAthlete(String email) {
+		return athleteService.getByEmail(email);
 	}
 
-	private Coach authCoach(String email) {
-		return cService.getByEmail(email);
+	private Coach getCoach(String email) {
+		return coachService.getByEmail(email);
 	}
 
 	private Boolean matchPassword(String email, String paramPassword) {
-		System.out.println(email + " " + paramPassword);
-		Athlete at = aService.getByEmail(email);
-		System.out.println(at);
-		return at.getPassword() == paramPassword;
+		Athlete athlete = athleteService.getByEmail(email);
+		System.out.println("[LoginController] : athlete.getPassword() : " + athlete.getPassword() + " : paramPassword : " + paramPassword);
+		return athlete.getPassword().equalsIgnoreCase(paramPassword);
 	}
 
 }
