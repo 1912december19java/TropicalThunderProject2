ce62455eec852394f6857890836dd3468b540729
Jordan Adkins <36913671+JordanAdkins@users.noreply.github.com>
2020-02-09 13:08:52 -0500

    Commented out search and edit modules

--- Frontend/SwoleMate/src/app/edit-profile/edit-profile.component.ts
+++ Frontend/SwoleMate/src/app/edit-profile/edit-profile.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { EditUser } from "../editUser";
+// import { Component, OnInit } from "@angular/core";
+// import { EditUser } from "../editUser";
 
-@Component({
-  selector: "app-edit-profile",
-  templateUrl: "./edit-profile.component.html",
-  styleUrls: ["./edit-profile.component.css"]
-})
-export class EditProfileComponent implements OnInit {
-  model = new EditUser("Jordan", "mail@email.com", "wasspord", "Athlete");
+// @Component({
+//   selector: "app-edit-profile",
+//   templateUrl: "./edit-profile.component.html",
+//   styleUrls: ["./edit-profile.component.css"]
+// })
+// export class EditProfileComponent implements OnInit {
+//   model = new EditUser("Jordan", "mail@email.com", "wasspord", "Athlete");
 
-  submitted = false;
+//   submitted = false;
 
-  onSubmit() {
-    this.submitted = true;
-  }
+//   onSubmit() {
+//     this.submitted = true;
+//   }
 
-  get diagnostic() {
-    return JSON.stringify(this.model) + this.submitted;
-  }
-  constructor() {}
+//   get diagnostic() {
+//     return JSON.stringify(this.model) + this.submitted;
+//   }
+//   constructor() {}
 
-  ngOnInit() {}
-}
+//   ngOnInit() {}
+// }
